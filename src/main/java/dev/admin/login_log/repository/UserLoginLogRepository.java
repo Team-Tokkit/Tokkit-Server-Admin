8ca166b555c793c961840c5fba8dea009f2d56c3
sengjun0624 <sengjun0624@naver.com>
2025-05-19 14:36:17 +0900

    :fire: delete: UserLoginLog.java

--- src/main/java/dev/admin/login_log/repository/UserLoginLogRepository.java
+++
@@ -1,7 +0,0 @@
-package dev.admin.login_log.repository;
-
-import dev.admin.login_log.entity.UserLoginLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserLoginLogRepository extends JpaRepository<UserLoginLog, Long> {
-}
