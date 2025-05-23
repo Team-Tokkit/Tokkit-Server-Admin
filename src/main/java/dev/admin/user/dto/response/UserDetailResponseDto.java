6222722ff4236602b0876680c60fa9d6c073608f
sengjun0624 <sengjun0624@naver.com>
2025-05-23 11:15:09 +0900

    :recycle: refactor: User Request,Response Dto 필드명 변경

--- src/main/java/dev/admin/user/dto/response/UserDetailResponseDto.java
+++ src/main/java/dev/admin/user/dto/response/UserDetailResponseDto.java
@@ -6,7 +6,7 @@ public record UserDetailResponseDto(
         @Schema(description = "사용자 ID", example = "1") Long id,
         @Schema(description = "이름", example = "홍길동") String name,
         @Schema(description = "이메일", example = "hong@example.com") String email,
-        @Schema(description = "전화번호", example = "010-1234-5678") String phone,
+        @Schema(description = "전화번호", example = "010-1234-5678") String phoneNumber,
         @Schema(description = "상태", example = "true") boolean isDormant,
         @Schema(description = "가입일", example = "2025-05-20") String createdAt,
         @Schema(description = "지갑 id", example = "1") long walletId
@@ -15,11 +15,11 @@ public record UserDetailResponseDto(
             Long id,
             String name,
             String email,
-            String phone,
+            String phoneNumber,
             boolean status,
             String createdAt,
             long walletId
     ) {
-        return new UserDetailResponseDto(id, name, email, phone, status, createdAt, walletId);
+        return new UserDetailResponseDto(id, name, email, phoneNumber, status, createdAt, walletId);
     }
 }
