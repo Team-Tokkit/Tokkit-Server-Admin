ee3d0b1883f06f8c91c4079bd6a0a003a3563574
sengjun0624 <sengjun0624@naver.com>
2025-05-23 15:07:45 +0900

    :fire: delete: 주석 제거

--- src/main/java/dev/admin/global/util/PasswordUtil.java
+++ src/main/java/dev/admin/global/util/PasswordUtil.java
@@ -3,15 +3,12 @@ package dev.admin.global.util;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class PasswordUtil {
-    // encoder를 매번 새로 만들지 않고 한 번만 재사용
     private static final BCryptPasswordEncoder ENCODER = new BCryptPasswordEncoder();
 
-    /** 비밀번호 암호화 */
     public static String encode(String rawPassword) {
         return ENCODER.encode(rawPassword);
     }
 
-    /** 비밀번호 검증 */
     public static boolean matches(String rawPassword, String encodedPassword) {
         return ENCODER.matches(rawPassword, encodedPassword);
     }
