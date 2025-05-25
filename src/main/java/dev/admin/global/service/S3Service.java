502419856892bd254aea071411a3471e34793583
jeongmin07262 <dlwjdals0726@gmail.com>
2025-05-25 15:47:11 +0900

    feat: S3에 바우처 업로드 기능 구현

--- src/main/java/dev/admin/global/service/S3Service.java
+++ src/main/java/dev/admin/global/service/S3Service.java
@@ -3,6 +3,7 @@ package dev.admin.global.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.core.ResponseBytes;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
@@ -11,6 +12,9 @@ import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.time.Duration;
 
@@ -25,7 +29,7 @@ public class S3Service {
     @Value("${cloud.aws.s3.bucket}")
     private String bucketName;
 
-    // S3에 이미지를 업로드하는 메서드
+    // S3의 presigned url 생성
     public URL generatePresignedUrl(String fileName, String contentType) {
         String key = buildKey(fileName);
 
@@ -41,6 +45,27 @@ public class S3Service {
         );
 
         return presignedRequest.url();
+    }
+
+    // S3에 이미지를 업로드하는 메서드
+    public String uploadImage(String fileName, MultipartFile file) throws IOException {
+        String contentType = file.getContentType();
+        URL presignedUrl = generatePresignedUrl(fileName, contentType);
+
+        try (InputStream inputStream = file.getInputStream()) {
+            HttpURLConnection connection = (HttpURLConnection) presignedUrl.openConnection();
+            connection.setDoOutput(true);
+            connection.setRequestMethod("PUT");
+            connection.setRequestProperty("Content-Type", contentType);
+            connection.getOutputStream().write(inputStream.readAllBytes());
+
+            int responseCode = connection.getResponseCode();
+            if (responseCode != HttpURLConnection.HTTP_OK) {
+                throw new RuntimeException("Failed to upload file, response code: " + responseCode);
+            }
+        }
+
+        return IMAGE_FOLDER + fileName;
     }
 
     // S3에서 이미지를 가져오는 메서드
