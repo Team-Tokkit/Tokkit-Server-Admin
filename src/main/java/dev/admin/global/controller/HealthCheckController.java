becc0ffb44b918f4f274e8c044b3c61686379046
sengjun0624 <sengjun0624@naver.com>
2025-05-27 23:06:32 +0900

    :recycle: refactor: Modify HealthCheckController

--- src/main/java/dev/admin/global/controller/HealthCheckController.java
+++ src/main/java/dev/admin/global/controller/HealthCheckController.java
@@ -1,9 +1,11 @@
 package dev.admin.global.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/admin-api")
 public class HealthCheckController {
 
 	@GetMapping("/health")
