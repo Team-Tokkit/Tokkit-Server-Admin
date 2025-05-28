bc6bc0547b60f3a0b1cf38580e3814b5ab57597a
이승준 <105282117+sengjun0624@users.noreply.github.com>
2025-05-28 17:35:16 +0900

    :sparkles: Feat: Add Unified Log Service (#42)
    * :sparkles: feat: Add Unified Log Service
    
    * :recycle: refactor: Modify DateTime Type

---
+++ src/main/java/dev/admin/unified_logs/dto/response/UnifiedLogResponseDto.java
@@ -0,0 +1,72 @@
+package dev.admin.unified_logs.dto.response;
+
+
+import java.time.LocalDateTime;
+
+import dev.admin.api_request_log.entity.ApiRequestLog;
+import dev.admin.login_log.entity.LoginLog;
+import dev.admin.system_error_log.entity.SystemErrorLog;
+import dev.admin.transaction.entity.Transaction;
+
+public record UnifiedLogResponseDto(
+	String logType,
+	String traceId,
+	Long userId,
+	Long merchantId,
+	LocalDateTime timestamp,
+	String summary,
+	String detail,
+	String statusOrSeverity
+) {
+	public static UnifiedLogResponseDto fromLoginLog(LoginLog log) {
+		return new UnifiedLogResponseDto(
+			"LOGIN",
+			log.getTraceId(),
+			log.getUserId(),
+			log.getMerchantId(),
+			log.getTimestamp(),
+			log.getEvent() + " - " + log.getIpAddress(),
+			log.getUserAgent(),
+			log.getSuccess() ? "SUCCESS" : "FAILURE"
+		);
+	}
+
+	public static UnifiedLogResponseDto fromApiRequestLog(ApiRequestLog log) {
+		return new UnifiedLogResponseDto(
+			"API",
+			log.getTraceId(),
+			log.getUserId(),
+			log.getMerchantId(),
+			log.getTimestamp(),
+			log.getMethod() + " " + log.getEndpoint(),
+			log.getQueryParams() + "\n" + log.getRequestBody(),
+			String.valueOf(log.getResponseStatus())
+		);
+	}
+
+	public static UnifiedLogResponseDto fromSystemErrorLog(SystemErrorLog log) {
+		return new UnifiedLogResponseDto(
+			"ERROR",
+			log.getTraceId(),
+			log.getUserId(),
+			log.getMerchantId(),
+			log.getTimestamp(),
+			log.getEndpoint() + " - " + log.getErrorMessage(),
+			log.getStackTrace(),
+			log.getSeverity().toString()
+		);
+	}
+
+	public static UnifiedLogResponseDto fromTransactionLog(Transaction log) {
+		return new UnifiedLogResponseDto(
+			"TRANSACTION",
+			log.getTraceId(),
+			null,
+			null,
+			log.getCreatedAt(),
+			log.getType() + " - " + log.getAmount(),
+			log.getDescription(),
+			log.getStatus().toString()
+		);
+	}
+}
