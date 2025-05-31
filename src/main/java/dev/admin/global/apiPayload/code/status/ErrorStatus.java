ef9d4fbe3414a09a3dd4a5bad46fb7b446bc933f
amy8883 <amy8883@naver.com>
2025-05-31 22:27:02 +0900

    FIX: Enum 정의 오류 수정 및 예외 처리 개선

--- src/main/java/dev/admin/global/apiPayload/code/status/ErrorStatus.java
+++ src/main/java/dev/admin/global/apiPayload/code/status/ErrorStatus.java
@@ -21,6 +21,9 @@ public enum ErrorStatus implements BaseErrorCode {
 	_UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "COMMON401", "인증이 필요합니다."),
 	_FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다."),
 
+	// Login
+	INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "A001", "비밀번호가 일치하지 않습니다."),
+
 	// Notice
 	NOTICE_NOT_FOUND(HttpStatus.NOT_FOUND, "NOTICE404", "공지사항을 찾을 수 없습니다."),
 
