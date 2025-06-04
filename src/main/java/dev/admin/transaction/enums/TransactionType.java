f06e606ee6248b783503a1fe9f8dcb21667b9aa5
amy8883 <amy8883@naver.com>
2025-06-04 17:07:48 +0900

    REFACTOR: admin -> admin-api 수정 및 TransactionType 추가

--- src/main/java/dev/admin/transaction/enums/TransactionType.java
+++ src/main/java/dev/admin/transaction/enums/TransactionType.java
@@ -1,5 +1,5 @@
 package dev.admin.transaction.enums;
 
 public enum TransactionType {
-    DEPOSIT, WITHDRAW, CONVERT, PURCHASE, REFUND, RECEIVE
+    DEPOSIT, WITHDRAW, CONVERT, PURCHASE, REFUND, RECEIVE,AUTO_CONVERT
 }
